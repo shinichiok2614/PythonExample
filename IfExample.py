dfaefccb75d75964698ce875eb492cd675a0f325
shinichiok2614 <phuonghoangit2614@gmail.com>
2023-01-13 11:09:30 +0700

    init

--- IfExample.py
+++
@@ -1,7 +0,0 @@
-a = 5
-if a > 3:
-    print('a>3')
-elif a<3:
-    print('a<3')
-else:
-    print('a=3')
--- PhepToanExample.py
+++
@@ -1,8 +0,0 @@
-print(3/2)
-print(3//2) #chia lay nguyen
-print(5%3)
-print(3*2)
-print(3**2)
-print(abs(-2.5))
-print(round(2.5))
-print(round(2.8))
